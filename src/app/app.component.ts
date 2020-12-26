67fe6084cf7167e82699505691ff4e62ccd7b440
Saket Gaonkar <saket2498@gmail.com>
2020-12-26 21:29:18 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'task2-favicon';
+  searchUrl: any;
+  // url = 'https://api.statvoo.com/favicon/?url=d2itechnology.com'
+  extractUrl = '';
+  displayUrl: any;
+  finalUrl = '';
+  // displayUrl2: any;
+  constructor() {
+
+  }
+
+  search(item) {
+    this.searchUrl = item;
+    console.log(this.searchUrl);
+    this.extractUrl = this.searchUrl.match(/www.(.*)/);
+    console.log(this.extractUrl[1]);
+    this.finalUrl = this.extractUrl[1];
+    this.displayUrl = `https://api.statvoo.com/favicon/?url=${this.extractUrl[1]}`
+    console.log(this.displayUrl)
+    // this.displayUrl2 = `http://f1.allesedv.com/16/${this.extractUrl[1]}`
+    // console.log(this.displayUrl2)
+    // this.finalUrl = this.displayUrl.match(/(.*),/)
+    // console.log(this.finalUrl[1])
+  }
 }
